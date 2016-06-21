6c5144413703b36b698073a10ef1cc641c3da757
Steve Thorn <sthorn@renovo1.com>
2016-06-21 10:34:46 -0400

    initial tests

--- index.js
+++ index.js
@@ -2,41 +2,47 @@ var express = require('express');
 var app = express();
 var url = require('url');
 var request = require('request');
-
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('port', (process.env.PORT || 9001));
+app.set('port', (process.env.PORT || 9001)); // in production, use the [PORT] env variable
 
+// Just for grins, show text when visiting root of the app
 app.get('/', function(req, res){
   res.send('It works!');
 });
 
+// All Slack Slash commands send a POST
 app.post('/post', function(req, res){
-  var parsed_url = url.format({
-    pathname: 'https://api.genius.com/search',
-    query: {
-      access_token: process.env.GENIUS_ACCESS,
-      q: req.body.text
-    }
-  });
+  // req.body.text is the slash command
 
-  request(parsed_url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var data = JSON.parse(body);
-      var first_url = data.response.hits[0].result.url;
+  // var parsed_url = url.format({
+  //   pathname: 'https://api.genius.com/search',
+  //   query: {
+  //     access_token: process.env.GENIUS_ACCESS,
+  //     q: req.body.text
+  //   }
+  // });
 
-      var body = {
-        response_type: "in_channel",
-        text: first_url
-      };
+  var first_url = "Whatever.";
 
-      res.send(body);
-    }
-  });
+  // request(parsed_url, function (error, response, body) {
+  //   if (!error && response.statusCode == 200) {
+  //     var data = JSON.parse(body);
+  //     var first_url = data.response.hits[0].result.url;
+
+       var body = {
+         response_type: "in_channel",
+         text: first_url
+       };
+
+       res.send(body);
+  //   }
+  // });
 });
 
+// Tell node which port to listen on
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
